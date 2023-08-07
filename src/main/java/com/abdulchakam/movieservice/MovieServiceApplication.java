2bdab717325fc8f23ad966e4185ea934bca0114c
abdulchakam <abdul.chakam@ist.id>
2023-08-07 13:42:11 +0700

    Enhance API Documentation Swagger

--- src/main/java/com/abdulchakam/movieservice/MovieServiceApplication.java
+++ src/main/java/com/abdulchakam/movieservice/MovieServiceApplication.java
@@ -1,10 +1,16 @@
 package com.abdulchakam.movieservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Movie RESTFul API", version = "1.0", description = "OpenAPI for Movie RESTFul API",
+				contact = @Contact(name = "Muhammad Abdul Chakam", email = "muhabdulchakam@gmail.com", url = "https://github.com/abdulchakam/movie-service-app")))
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
